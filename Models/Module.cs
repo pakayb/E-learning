370ce38bc5af377e5597341715caaa089fff6dca
pakayb <pakayb@gmail.com>
2020-01-13 11:02:31 +0100

    Building base models

--- Models/Module.cs
+++ Models/Module.cs
@@ -2,15 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace ELearningV2.Models
 {
-    public class Lesson
+    public class Module
     {
         public long Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public long ModuleId { get; set; }
     }
 }
